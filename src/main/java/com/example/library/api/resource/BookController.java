5402c7c732c6c68adefe0b781bb552aea8417293
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-06-12 15:52:20 -0300

    ModelMapper added to BookController

--- src/main/java/com/example/library/api/resource/BookController.java
+++ src/main/java/com/example/library/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.example.library.api.resource;
 import com.example.library.api.dto.BookDTO;
 import com.example.library.entities.Book;
 import com.example.library.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,21 +12,20 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service) {
+    public BookController(BookService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO bookDTO){
-        Book book = Book.builder().author(bookDTO.getAuthor()).title(bookDTO.getTitle()).isbn(bookDTO.getIsbn()).build();
+        Book book = modelMapper.map(bookDTO, Book.class);
+
         book = service.save(book);
-        return BookDTO.builder()
-                .id(book.getId())
-                .author(book.getAuthor())
-                .title(book.getTitle())
-                .isbn(book.getIsbn())
-                .build();
+
+        return modelMapper.map(book, BookDTO.class);
     }
 }
