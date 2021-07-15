14933e6d5bd26524fdb8fe5c0270b16a8249af4b
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-07-15 15:54:55 -0300

    Final commit Application

--- src/main/java/com/example/library/service/ScheduleService.java
+++ src/main/java/com/example/library/service/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.example.library.service;
 
 import com.example.library.entities.Loan;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 
 @Service
 @EnableScheduling
+@RequiredArgsConstructor
 public class ScheduleService {
 
     private static final String CRON_LATE_LOANS = "0 0 0 1/1 * ?";
@@ -20,11 +22,6 @@ public class ScheduleService {
 
     private final LoanService loanService;
     private final EmailService emailService;
-
-    public ScheduleService(LoanService loanService, EmailService emailService) {
-        this.loanService = loanService;
-        this.emailService = emailService;
-    }
 
     @Scheduled(cron = CRON_LATE_LOANS)
     public void sendMailToLateLoans(){
--- src/main/java/com/example/library/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/library/service/impl/EmailServiceImpl.java
@@ -1,15 +1,33 @@
 package com.example.library.service.impl;
 
 import com.example.library.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.mail.default-sender}")
+    private String sender;
+
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendEmails(String msg, List<String> emailsList) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        String[] emails = emailsList.toArray(new String[emailsList.size()]);
 
+        mailMessage.setFrom(sender);
+        mailMessage.setSubject("Livro com empréstimo atrasado");
+        mailMessage.setText(msg);
+        mailMessage.setTo(emails);
+
+        javaMailSender.send(mailMessage);
     }
 }
